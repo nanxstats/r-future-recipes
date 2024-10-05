5bf8ec064adb16523451771dd7c0ee2ed98094dd
Nan Xiao <me@nanx.me>
2024-10-05 17:32:35 -0400

    Use Cascadia Code

--- images/banner.sh
+++ images/banner.sh
@@ -4,11 +4,11 @@ magick -size 2048x734 \
   -define gradient:angle=330 \gradient:#500c7f-#ff5988 \
   -gravity center \
   -pointsize 100 \
-  -font 'Jetbrains-Mono-Bold' \
+  -font 'Cascadia-Code-SemiBold' \
   -fill white \
   -annotate +0-80 'r-future-recipes' \
   -pointsize 36 \
-  -font 'Jetbrains-Mono-SemiBold' \
+  -font 'Cascadia-Code-SemiBold' \
   -annotate +0+60 '\%dofuture\% Operator · Nested Foreach Loops' \
   -annotate +0+130 'Future Topologies · Best Practices' \
-  png:- | pngquant - --force --output banner.png
+  png:- | pngquant - --force --output images/banner.png
